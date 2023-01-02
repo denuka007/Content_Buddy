5fb43604f9442b555ddaefe4a278f1a46b88e1cb
denuka007 <115033973+denuka007@users.noreply.github.com>
2023-01-02 18:10:40 +0530

    Update sql_helper.dart

--- lib/sql_helper.dart
+++ lib/sql_helper.dart
@@ -17,7 +17,7 @@ class SQLHelper {
 
   static Future<sql.Database> db() async {
     return sql.openDatabase(
-      'dbtech.db',
+      'cb.db',
       version: 1,
       onCreate: (sql.Database database, int version) async {
         await createTables(database);
